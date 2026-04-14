57aae2c2ee5df80d5a94148e601c1092ac9d1e5b
DuMMy9813 <anuzr88@gmail.com>
2026-04-14 13:13:36 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,8 @@
 
-CREATE TABLE messages (
-    Id INT PRIMARY KEY,
-    Content TEXT
+CREATE TABLE uw_messages (
+    uw_Id INT PRIMARY KEY,
+    uw_Username TEXT NOT NULL,
+    uw_Content TEXT NOT NULL
 );
+
+CREATE SEQUENCE uw_msgSeq;
